32b142e771a7487a11d42a489dd747a97f7b7233
CardoPixel <cardo.pixelado@gmail.com>
2024-08-30 12:16:07 -0300

    fix: :bug: fixing infinite redirect bug

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,16 @@
 // File: app/checkout/page.tsx
-import React from 'react';
+"use client"
+import React, { useEffect } from 'react';
+import { useMetadataStore } from '@/store/usePoCStore'; // Import the zustand store
 
 const CheckoutPage: React.FC = () => {
+    const { setMetadata } = useMetadataStore(); // Destructure the setMetadata function from the zustand store
+
+    useEffect(() => {
+        // Clear the metadata and checkoutUrl values when the component mounts
+        setMetadata({ orderId: '', userId: '', amount: 0 }, ''); // Example values, replace with actual data
+    }, []); // Empty dependency array ensures this effect runs only once when the component mounts
+
     return (
         <div>
             <h2>Mock Checkout Page</h2>
