d13da14533a32306fe1240213d6cf4be02f461a0
CardoPixel <cardo.pixelado@gmail.com>
2024-08-30 09:49:30 -0300

    feat: :sparkles: Added mock checkout page

--- app/api/sns-handler/route.ts
+++ app/api/sns-handler/route.ts
@@ -1,33 +1,27 @@
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
-    console.log(`📩 Recieved POST request: ${JSON.stringify(request)}`);
+    console.log('📩 Recieved POST request:', request); // Logging the raw POST request
     try {
         const body = await request.json();
         const type = body.Type; // Access the 'Type' field from the SNS message
-
         console.log('🔎 Received SNS message type:', type); // Logging the SNS message type
 
         if (type === 'SubscriptionConfirmation') {
             const url = body.SubscribeURL;
             console.log('🛂 Subscription confirmation URL:', url); // Logging the subscription URL
-
             // Perform the subscription confirmation if required
             await fetch(url);
             console.log('☑️ Subscription confirmed successfully');
-
             return NextResponse.json({ message: '✅ Subscription confirmed' });
         } else if (type === 'Notification') {
             console.log(`📥 Received Notification message: ${JSON.stringify(body)}`);
-
             const messageContent = body.Message;
             const parsedMessage = JSON.parse(messageContent);
             const metadata = parsedMessage.metadata;
             const checkoutUrl = parsedMessage.checkoutUrl;
-
             console.log('📦 Extracted metadata: ', metadata); // Logging extracted metadata
             console.log('📋 Checkout URL: ', checkoutUrl); // Logging checkout URL
-
             return NextResponse.json({ metadata, checkoutUrl });
         } else {
             console.warn('⚠️ Unhandled SNS message type:', type); // Logging unhandled message type
---
+++ app/checkout/page.tsx
@@ -0,0 +1,14 @@
+// File: app/checkout/page.tsx
+import React from 'react';
+
+const CheckoutPage: React.FC = () => {
+    return (
+        <div>
+            <h2>Mock Checkout Page</h2>
+            <p>This is a mock checkout page for testing purposes.</p>
+            {/* Add checkout form or any other checkout-related content here */}
+        </div>
+    );
+};
+
+export default CheckoutPage;
