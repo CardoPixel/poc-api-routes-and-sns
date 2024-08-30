d992f5e89d69638d518f509376c9638b206a33ef
CardoPixel <cardo.pixelado@gmail.com>
2024-08-30 10:40:57 -0300

    refactor: :loud_sound: Updated server-side logs

--- app/api/sns-handler/route.ts
+++ app/api/sns-handler/route.ts
@@ -8,34 +8,45 @@ export async function POST(request: Request) {
         clients.forEach((client) => client.res.write(`data: ${log}\n\n`));
     };
 
+    console.log('📩 Recieved POST request:', request); // Logging the raw POST request
     logToClients("📩 Received POST request: " + JSON.stringify(request));
 
     try {
         const body = await request.json();
+        console.log('📋 Parsed body: ', JSON.stringify(body));
         logToClients("📋 Parsed body: " + JSON.stringify(body));
 
         const type = body.Type;
+        console.log('🔎 Received SNS message type:', type);
         logToClients("🔎 Received SNS message type: " + type);
 
         if (type === "SubscriptionConfirmation") {
             const url = body.SubscribeURL;
+            console.log('🛂 Subscription confirmation URL:', url); // Logging the subscription URL
             logToClients("🛂 Subscription confirmation URL: " + url);
+            // Perform the subscription confirmation if required
             await fetch(url);
+            console.log('☑️ Subscription confirmed successfully');
             logToClients("☑️ Subscription confirmed successfully");
             return NextResponse.json({ message: "✅ Subscription confirmed" });
         } else if (type === "Notification") {
             const messageContent = JSON.parse(body.Message);
+            console.log(`📥 Received Notification message: ${JSON.stringify(messageContent)}`);
             logToClients(`📥 Received Notification message: ${JSON.stringify(messageContent)}`);
             const metadata = messageContent.metadata;
             const checkoutUrl = messageContent.checkoutUrl;
+            console.log('📦 Extracted metadata: ', metadata); // Logging extracted metadata
+            console.log('📋 Checkout URL: ', checkoutUrl); // Logging checkout URL
             logToClients("📦 Extracted metadata: " + metadata);
             logToClients("📋 Checkout URL: " + checkoutUrl);
             return NextResponse.json({ metadata, checkoutUrl });
         } else {
+            console.warn('⚠️ Unhandled SNS message type:', type); // Logging unhandled message type
             logToClients("⚠️ Unhandled SNS message type: " + type);
             return NextResponse.json({ message: "⚠️ Unhandled SNS message type" });
         }
     } catch (error) {
+        console.error('❌ Error processing SNS message:', error); // Logging processing error
         logToClients("❌ Error processing SNS message: " + (error as Error).toString());
         return NextResponse.json({ error: "❌ Invalid SNS message" });
     }
